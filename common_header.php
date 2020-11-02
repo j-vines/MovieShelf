b5a1ad834552d2f2cf8e1df0d26253b4bd79b6c2
Jack <3jackvines@gmail.com>
2020-11-02 03:05:40 -0500

    Created sign up page, replaced html files with php files

--- common_header.php
+++ common_header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Welcome to MovieShelf!</title>
-<link rel="stylesheet" href="header_styles.css">
+<link rel="stylesheet" href="css/header_styles.css">
 </head>
 	
 <body>
